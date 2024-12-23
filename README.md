112b7088d1b621e4d57d5d52af76059a131aa742
deepakkhokhar1313 <deepakkhokhar1313@gmail.com>
2024-12-23 14:50:48 +0000

    data ingestion training pipeline implemented

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Datascience_Project_1
+# End to End Datascience project
+
+## Workflows - ML-Pipeline
+
+# stages:
+1. Data Inestion
+2. Data Validation
+3. Data Transformation
+4. Model trainer
+5. Model Evaluation
+
+## WorkFlows
+
+1. Update the config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src.Datascience_1.logging import logger
+from src.Datascience_1.pipeline.data_ingestion_pipeline import (DataIngestionTrainingPipeline)
 
-logger.info("Welcome to datascience project")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
